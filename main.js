b3f334d58b7de9830a35de10821806202394a10a
kanistra <kanistra50@gmail.com>
2019-01-27 10:03:52 +0200

    Modules imported

--- main.js
+++ main.js
@@ -1,2 +1,28 @@
-const Notifier = require('/js/notifier');
-const Subscriber = require('./js/subscriber');
+const Notifier = require('notifier');
+const Subscriber = require('subscriber');
+const Event = require('event');
+
+const jsConference = new Event('JS Conference');
+const angularConference = new Event('Angular Conference');
+const reactConference = new Event('React Conference');
+
+const john = new Subscriber('John');
+const micky = new Subscriber('Micky');
+const leon = new Subscriber('Leon');
+
+const admin = new Notifier();
+jsConference.subscribe(admin);
+angularConference.subscribe(admin);
+reactConference.subscribe(admin);
+
+jsConference.subscribe(john);
+jsConference.subscribe(micky);
+jsConference.subscribe(leon);
+angularConference.subscribe(micky);
+reactConference.subscribe(leon);
+
+jsConference.event('15 March');
+angularConference.event('20 June');
+reactConference.event('1 May');
+
+console.log(admin.notifier);
